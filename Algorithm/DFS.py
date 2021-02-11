804ded3b588f4e5cc0a3bd673060f33155e912a1
JUNHYOEKCHO <mo223772@naver.com>
2021-02-12 01:40:44 +0900

    DFS Algorithm example code

--- Algorithm/DFS.py
+++ Algorithm/DFS.py
@@ -1,5 +1,3 @@
-# 파이썬으로 그래프를 표현하는 방법은 딕셔너리와 리스트 자료 구조를 활용하여 표현 가능
-
 graph = dict()
 
 graph['A'] = ['B', 'C']
@@ -13,28 +11,20 @@ graph['H'] = ['C']
 graph['I'] = ['C', 'J']
 graph['J'] = ['I']
 
-def bfs(graph, start_node):
-    # 방문한 queue
-    visited = list()
-    # 방문이 필요한 queue
-    need_visit = list()
+# need_visit을 stack, visted를 queue로 짜면 DFS 구현 가능
+# stack이니까 뒤에 있는 데이터 먼저 탐지
+def dfs(graph, start_node):
+    visited, need_visit = list(), list()
 
-    # 어떤 노드부터 시작할지 큐에 담아놓고 시작
     need_visit.append(start_node)
 
-    # 더 이상 방문할 노드가 없다면 종료
     while need_visit:
-        # 그냥 팝하면 맨 마지막께 뽑힘.
-        # 숫자를 넣어주면 해당 인덱스가 뽑혀져 나옴
-        node = need_visit.pop(0)
-
-        # 만약에 뽑아온 노드가 visited 큐에 없으면?
+        # 그냥 pop을 하면 맨 뒤의 원소를 그냥 꺼내오게 된다.
+        node = need_visit.pop()
         if node not in visited:
-            # 방문한 큐에 더해주고
             visited.append(node)
-            # 방문 해야할 노드를 해당 노드와 연결된 친구들을 더해줌
             need_visit.extend(graph[node])
 
     return visited
 
-print(bfs(graph, 'A'))
+print(dfs(graph, 'A'))
